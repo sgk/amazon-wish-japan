6c89bcf4ef02fc1d9083a87e4867d0ddbed62ab5
Shigeru KANEMOTO <sgk@accense.com>
2011-04-18 09:03:35 +0900

    http://amazon-wish-japan.appspot.com/

---
+++ amazon.py
@@ -0,0 +1,84 @@
+#vim:fileencoding=utf-8
+
+import urllib2
+import re
+
+WISHID_LIST_URL = u'http://www.amazon.co.jp/gp/registry/search.html?type=wishlist&field-name=【被災地】&page=%d'
+RE_LIST_COUNT = re.compile(u'- (\d+) 件が一致しました。')
+RE_WISHID = re.compile('<a href="/registry/wishlist/([0-9A-Z]+)">')
+
+WISH_LIST_URL = u'http://www.amazon.co.jp/registry/wishlist/%s?reveal=all&filter=all&sort=date-added&layout=compact'
+RE_OWNER = re.compile('<tr><th>For</th><td class="profileInfoField" id="profile-name-Field">([^<]*)</td></tr>')
+RE_ALL_ITEMS_COUNT = re.compile('<span id="topItemCount">(\d+)</span>')
+RE_WISH_ITEMS_COUNT = re.compile('<span class="regListCount">(\d+)</span>')
+RE_ITEM = re.compile('<td class="tiny">(.*)</td>\s*<td align="center" class="tiny">(\d*)</td>\s*<td align="center" class="tiny">(\d*)</td>\s*<td align="center" class="tiny">([^<]*)</td>')
+RE_PRICE = re.compile('\.JPY\.(\d+)"')
+
+WISH_LIST_PAGE = u'http://www.amazon.co.jp/registry/wishlist/%s'
+
+RE_ENTITY = re.compile('&#(\d+);')
+
+def entity_helper(m):
+  id = m.group(1)
+  try:
+    return unichr(int(id))
+  except:
+    return '&#%s;' % id
+
+def decode_entity(s):
+  return RE_ENTITY.sub(entity_helper, s)
+
+def get_page_text(url):
+  page = urllib2.urlopen(url.encode('utf-8'))
+  page = page.read()
+  page = page.decode('cp932')
+  return page
+
+def get_wishid_list():
+  page = 0
+  while True:
+    page += 1
+    text = get_page_text(WISHID_LIST_URL % page)
+    m = RE_LIST_COUNT.search(text)
+    count = int(m.group(1))
+    for m in RE_WISHID.finditer(text):
+      yield m.group(1)
+    if page * 25 >= count:
+      break
+
+def get_wish_list(id):
+  text = get_page_text(WISH_LIST_URL % id)
+  m = RE_OWNER.search(text)
+  owner = decode_entity(m.group(1))
+
+  m = RE_ALL_ITEMS_COUNT.search(text)
+  all_items = int(m.group(1)) if m else 0
+  m = RE_WISH_ITEMS_COUNT.search(text)
+  wish_items = int(m.group(1)) if m else 0
+  got_items = all_items - wish_items
+
+  wish_amount = got_amount = 0
+  # XXX 複数ページの処理が必要。
+  for m in RE_ITEM.finditer(text):
+    mm = RE_PRICE.search(m.group(1))
+    price = int(mm.group(1)) if mm else 0
+    all_ = int(m.group(2))
+    got = int(m.group(3))
+    priority = m.group(4)
+    if all_ > got:
+      wish_amount += (all_ - got) * price
+    got_amount += got * price
+
+  return owner, wish_items, got_items, wish_amount, got_amount
+
+def wish_list_page_from_id(id):
+  return WISH_LIST_PAGE % id
+
+def main():
+  for id in get_wishid_list():
+    owner, wi, gi, wa, ga = get_wish_list(id)
+    print owner.encode('utf-8')
+    print wi, gi, wa, ga
+
+if __name__ == '__main__':
+  main()
---
+++ main.py
@@ -0,0 +1,81 @@
+#vim:fileencoding=utf-8
+
+from flask import (
+  Flask, render_template,
+)
+app = Flask(__name__)
+app.debug = True
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+
+import datetime
+import time
+import logging
+import amazon
+
+CRAWL_INTERVAL = 5 * 60
+
+################################################################################
+
+VERY_OLD = datetime.datetime.fromtimestamp(0)
+
+class WishId(db.Model):
+  # key: Wishlist id
+  updated = db.DateTimeProperty(auto_now=True)
+  page_updated = db.DateTimeProperty(default=VERY_OLD)
+
+class WishPage(db.Model):
+  # key: Wishlist id
+  owner_name = db.StringProperty()
+  wish_items = db.IntegerProperty()
+  got_items = db.IntegerProperty()
+  wish_amount = db.IntegerProperty()
+  got_amount = db.IntegerProperty()
+
+################################################################################
+
+# XXX 最終更新時刻を表示する。
+# XXX 重要度を記録するか？
+
+# cronから起動する。
+@app.route('/update_wishid_list')
+def update_wishid_list():
+  for id in amazon.get_wishid_list():
+    WishId.get_or_insert(id)
+    WishPage.get_or_insert(
+      id,
+      owner_name=id,
+    )
+  # XXX 古くなったまま現れない物は削除する。
+  return ''
+
+# cronから起動する。
+# 30秒の時間制限いっぱいまで、クロールする。
+@app.route('/update_wish_pages')
+def update_wish_pages():
+  older_than = datetime.datetime.fromtimestamp(time.time() - CRAWL_INTERVAL)
+  ids = WishId.all().filter('page_updated <', older_than).order('page_updated')
+  for id in ids:
+    try:
+      owner, wi, gi, wa, ga = amazon.get_wish_list(id.key().name())
+    except:
+      # XXX 削除する。
+      continue
+    WishPage(
+      key_name=id.key().name(),
+      wishid=id,
+      owner_name=owner,
+      wish_items=wi,
+      got_items=gi,
+      wish_amount=wa,
+      got_amount=ga,
+    ).put()
+    id.page_updated = datetime.datetime.now()
+    id.put()
+  return ''
+
+@app.route('/')
+def top():
+  pages = WishPage.all().order('-wish_amount')
+  return render_template('top.html', pages=pages)
