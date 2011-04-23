ec32c817e7dd238d09d816b6a9927cc9bf183313
Shigeru KANEMOTO <sgk@accense.com>
2011-04-23 19:46:12 +0900

    Google URL短縮サービス。

---
+++ shorten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#vim:fileencoding=utf-8
+
+import urllib2
+import json
+
+GOOGLE_API_URL = 'https://www.googleapis.com/urlshortener/v1/url'
+
+def google(url, apikey=None):
+  api = GOOGLE_API_URL
+  if apikey:
+    api += '?key=' + apikey
+
+  data = json.dumps({'longUrl': url})
+  headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+  request = urllib2.Request(api, data, headers)
+  response = urllib2.urlopen(request).read()
+  response = json.loads(response)
+  return response.get('id', None)
+
+if __name__ == '__main__':
+  import sys
+  for url in sys.argv[1:]:
+    print shorten(url)
